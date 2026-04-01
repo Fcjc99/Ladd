190e7315807f0756af596a448ab27a8d5a27add3
Fcjc99 <144532599+Fcjc99@users.noreply.github.com>
2026-04-01 13:31:22 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   const [players, setPlayers] = useState([]);
-  const SHEET_URL = "https://opensheet.elk.sh/1zoH3KnR8sAh51XwOo5rDdK51OqcI5SXG1F_LTvtmTGk/App%20Feed";
+  const SHEET_URL = "https://opensheet.elk.sh/1lRG3_Tdfi95eu8LyLM9e8fic_IB8mpRj2s9YzOGiIDQ/App%20Feed";
 
   useEffect(() => {
     fetch(SHEET_URL)
